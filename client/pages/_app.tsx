a0cfe1603fb54c51a69ee0003df6bd1eeff4851d
tonyw0527 <tonyw0527@gmail.com>
2021-02-27 00:34:17 +0900

    Add styled-components

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { useDarkMode } from "../hooks/useDarkMode";
+import { lightTheme, darkTheme } from "../styles/theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-styles";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+
+  if (!componentMounted) {
+    return <div></div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Next App</title>
+      </Head>
+
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default App;
